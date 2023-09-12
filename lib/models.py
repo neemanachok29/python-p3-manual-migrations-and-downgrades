33992c125f1269ddbfb14d2715aa814940d1f016
Neema Nachok <neemanachok29@gmail.com>
2023-09-12 13:50:33 +0300

    code

--- lib/models.py
+++ lib/models.py
@@ -1,32 +1,23 @@
 import os
 import sys
-
 sys.path.append(os.getcwd())
-
-
 from datetime import datetime
-
 from sqlalchemy import create_engine, desc
 from sqlalchemy import (CheckConstraint, UniqueConstraint,
     Column, DateTime, Integer, String)
-
 from sqlalchemy.ext.declarative import declarative_base
-
 engine = create_engine('sqlite:///migrations_test.db')
-
 Base = declarative_base()
-
 class Student(Base):
     __tablename__ = 'students'
-
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
     email = Column(String(55))
     grade = Column(Integer())
     birthday = Column(DateTime())
     enrolled_date = Column(DateTime(), default=datetime.now())
-
     def __repr__(self):
         return f"Student {self.id}: " \
             + f"{self.name}, " \
+            + f"Grade {self.grade}"
             + f"Grade {self.grade}"
